7837fd76a0bfbce166523a5146f6874a4cf32e9a
VictorZionFranca <vzion435@gmail.com>
2025-04-14 17:38:14 -0300

    atualizando

--- projeto-ead/src/firebase/config.ts
+++ projeto-ead/src/firebase/config.ts
@@ -1,19 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7cFFbka-dN796_DSZZRKXSNIEww9Tpcg",
-  authDomain: "sistema-ead-1f362.firebaseapp.com",
-  projectId: "sistema-ead-1f362",
-  storageBucket: "sistema-ead-1f362.firebasestorage.app",
-  messagingSenderId: "608974026947",
-  appId: "1:608974026947:web:ee95ae6ff6c95ff49e8a76",
-  measurementId: "G-BYLJPR336C"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
